89e2ff0b3a2632134103bbd59592945046cf7c2a
MrRabbit <1993iweb@gmail.com>
2021-02-06 11:43:22 +0800

    feat: menu初始简单样式增加

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,19 @@ function App() {
         </MenuItem>
         <MenuItem index={2}>cool link3</MenuItem>
       </Menu>
+
+      <Menu
+        defaultIndex={0}
+        mode="vertical"
+        onSelect={(index) => {
+          console.log(index)
+        }}>
+        <MenuItem index={0}>cool link</MenuItem>
+        <MenuItem index={1} disabled>
+          cool link2
+        </MenuItem>
+        <MenuItem index={2}>cool link3</MenuItem>
+      </Menu>
       <Button disabled>按钮</Button>
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
         hello
