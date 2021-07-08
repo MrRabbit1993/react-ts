9ce2c04c9b361412b4167d9f4021d43bd0b9e4ab
MrRabbit <1993iweb@gmail.com>
2021-07-08 18:28:27 +0800

    feat: 使用react-transition-group 实现动画

--- src/components/Menu/sub-menu.tsx
+++ src/components/Menu/sub-menu.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState, FunctionComponentElement, useContext, cloneElement } from 'react'
 import classNames from 'classnames'
+import { CSSTransition } from 'react-transition-group'
 import { MenuContext } from './context'
 import { SubMenuProps, MenuItemProps } from './type'
 import { setTimeout } from 'timers'
@@ -47,7 +48,11 @@ const SubMenu: FC<SubMenuProps> = (props) => {
         console.error('Waring:SubMenu has a child which is not a MenuItem')
       }
     })
-    return <ul className={subMenuClasses}>{childrenComponent}</ul>
+    return (
+      <CSSTransition in={menuOpen} timeout={300} classNames="zoom-in-top" appear>
+        <ul className={subMenuClasses}>{childrenComponent}</ul>
+      </CSSTransition>
+    )
   }
   return (
     <li key={index} className={classes} {...mouseEvents}>
