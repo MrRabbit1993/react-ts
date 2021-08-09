6f8ceb11fcef03cf685d6c09186d0b5212e43b71
MrRabbit <1993iweb@gmail.com>
2021-08-09 14:41:15 +0800

    feat: 文件组件更新、

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -8,6 +8,19 @@ const Upload: FC<UploadProps> = (props) => {
   const { action, onProgress, onError, onSuccess, beforeUpload, onChange, defaultFileList, onRemove } = props
   const fileInput = useRef<HTMLInputElement>(null)
   const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList || [])
+
+  // 更新文件列表
+  const uploadFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
+    setFileList((prevList) => {
+      return prevList.map((file) => {
+        if (file.uid === updateFile.uid) {
+          return { ...file, ...updateObj }
+        } else {
+          return file
+        }
+      })
+    })
+  }
   const handlerClick = () => {
     if (fileInput.current) {
       fileInput.current.click()
@@ -41,6 +54,15 @@ const Upload: FC<UploadProps> = (props) => {
     })
   }
   const postFile = (file: File) => {
+    let _file: UploadFile = {
+      uid: Date.now() + 'upload-file',
+      status: 'ready',
+      name: file.name,
+      size: file.size,
+      percent: 0,
+      raw: file
+    }
+    setFileList([_file, ...fileList])
     const formData = new FormData()
     formData.append(file.name, file)
     axios
@@ -51,16 +73,19 @@ const Upload: FC<UploadProps> = (props) => {
         onDownloadProgress: (e) => {
           let percentage = Math.round((e.loaded * 100) / e.total) || 0
           if (percentage < 100) {
+            uploadFileList(_file, { percent: percentage, status: 'uploading' })
             onProgress?.(percentage, file)
           }
         }
       })
       .then((resp) => {
         console.log(resp)
+        uploadFileList(_file, { status: 'success', response: resp.data })
         onSuccess?.(resp.data, file)
         onChange?.(file)
       })
       .catch((err) => {
+        uploadFileList(_file, { status: 'error', error: err })
         onError?.(err, file)
         onChange?.(file)
       })
